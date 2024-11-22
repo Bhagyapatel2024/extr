b01c00512a3f3fe795dee5401738b6a6eebc802c
Bhagyapatel2024 <bhagya290405@gmail.com>
2024-11-22 13:16:57 +0530

    Update user.ts

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,32 +1,26 @@
 export interface User {
-  Invoices: [
-    {
-      SerialNumber: string;
-      CustomerName: string;
-      ProductName: string;
-      Quantity: number;
-      Tax: number;
-      TotalAmount: number;
-      Date: string; // Format: YYYY-MM-DD
-    }
-  ];
+  Invoices: {
+    SerialNumber: string;
+    CustomerName: string;
+    ProductName: string;
+    Quantity: number;
+    Tax: number;
+    TotalAmount: number;
+    Date: string; // Format: YYYY-MM-DD
+  }[];
 
-  Product: [
-    {
-      Name: string;
-      Quantity: number;
-      UnitPrice: number;
-      Tax: number;
-      PriceWithTax: number;
-      Discount: number;
-    }
-  ];
+  Product: {
+    Name: string;
+    Quantity: number;
+    UnitPrice: number;
+    Tax: number;
+    PriceWithTax: number;
+    Discount: number;
+  }[];
 
-  Customer: [
-    {
-      CustomerName: string;
-      PhoneNumber: string;
-      TotalPurchaseAmount: number;
-    }
-  ];
+  Customer: {
+    CustomerName: string;
+    PhoneNumber: string;
+    TotalPurchaseAmount: number;
+  }[];
 }
